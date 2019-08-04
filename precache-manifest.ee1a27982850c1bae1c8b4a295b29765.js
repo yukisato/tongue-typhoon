a2150c893644d70b507f8c6ad77b2e687c567527
Yuki Sato <yuki.sato.dev@gmail.com>
2019-08-04 00:44:53 -0700

    init

---
+++ precache-manifest.ee1a27982850c1bae1c8b4a295b29765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6e6cdcdd0fafc5e45dc52491ea3e6e",
+    "url": "/tongue-typhoon/index.html"
+  },
+  {
+    "revision": "491e82f57769829d344c",
+    "url": "/tongue-typhoon/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/tongue-typhoon/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "491e82f57769829d344c",
+    "url": "/tongue-typhoon/static/js/main.e0914ac1.chunk.js"
+  },
+  {
+    "revision": "4a76b12537ca6af4b122",
+    "url": "/tongue-typhoon/static/js/runtime~main.f3b3f2cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tongue-typhoon/static/media/logo.5d5d9eef.svg"
+  }
+]);
