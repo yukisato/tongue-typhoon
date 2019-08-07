97be0b0dc008b8649235c9f10374db39ca76c2d1
Yuki Sato <yuki.sato.dev@gmail.com>
2019-08-07 15:09:58 -0700

    chore: rewrite from a React component class to a function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 import './App.css';
 import tongueTwisters from './tongue-source.json';
 
-class App extends React.Component<{}, {selectedTwister: string}> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      selectedTwister: '',
-    }
-
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App: React.FC = (): ReactElement => {
+  const [selectedTwister, setSelectedTwister] = useState('');
   
-  handleClick() {
+  const handleClick = () => {
     let randomIndex = Math.floor(Math.random() * tongueTwisters.length + 1);
-    this.setState({selectedTwister: tongueTwisters[randomIndex].content});
+    setSelectedTwister(tongueTwisters[randomIndex].content);
   }
 
-  render() {
-      return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            { this.state.selectedTwister }
-          </p>
-          <button onClick={ this.handleClick }>Show a tongue twister</button>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          { selectedTwister }
+        </p>
+        <button onClick={ handleClick }>Show a tongue twister</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
